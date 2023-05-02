872da05815bd3ed80ad13ba8197617f061c33f0f
YuseiMorimoto <yusei0509@icloud.com>
2023-05-02 18:34:05 +0900

    ⚗️ 試しにクリックイベントを書いてみました。

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,12 +1,22 @@
 import { Inter } from "next/font/google";
 import { Main } from "@/components/Main";
 import { Header } from "@/components/Header";
+import { useCallback } from "react";
+import Link from "next/link";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const handleClick = useCallback((e) => {
+    e.preventDefault();
+    // alert(123);
+  });
+
   return (
     <div>
       <Header />
+      <Link href="/about" onClick={handleClick}>
+        ボタン
+      </Link>
       <Main page="index" />;
     </div>
   );
