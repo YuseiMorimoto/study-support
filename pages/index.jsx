8531404fa6e60bc2a7baaa24ad63bdb34fffdfb9
YuseiMorimoto <yusei0509@icloud.com>
2023-05-03 14:55:12 +0900

    ✨ カウンターを設置してuseStateを利用してみました。

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,32 +1,38 @@
 import { Inter } from "next/font/google";
 import { Main } from "@/components/Main";
 import { Header } from "@/components/Header";
-import { useCallback, useEffect } from "react";
+import { useEffect, useState } from "react";
 import Link from "next/link";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  const handleClick = useCallback((e) => {
-    e.preventDefault();
-    // alert(123);
-  });
+  // 前の状態を用いた処理をしたいときには
+  // setStateの中は関数で書く
+  // 必ず関数を利用する必要はない。
+  const [count, setCount] = useState(1);
+
+  const handleClick = (e) => {
+    setCount((count) => count + 1);
+    setCount((count) => count + 1);
+  };
 
   // マウント時の処理
   useEffect(() => {
-    console.log("マウント時");
+    // console.log("マウント時");
 
     // アンマウント時
     return () => {
-      console.log("アンマウント時");
+      // console.log("アンマウント時");
     };
   }, []);
 
   return (
     <div>
       <Header />
-      <Link href="/about" onClick={handleClick}>
+      <h1 className="text-center">{count}</h1>
+      <button className="content-center" onClick={handleClick}>
         ボタン
-      </Link>
+      </button>
       <Main page="index" />;
     </div>
   );
