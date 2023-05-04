36cebbe81bbcbfc9f7de533fbb473580c0ac0a3b
YuseiMorimoto <yusei0509@icloud.com>
2023-05-04 17:33:53 +0900

    ✨ コンポーネント間のstateの共通化について勉強

--- components/Links/index.jsx
+++ components/Links/index.jsx
@@ -6,36 +6,10 @@ const inter = Inter({ subsets: ["latin"] });
 // データと処理の部分を分ける。
 // mapは一意のkeyを持たせる。
 
-const ITEMS = [
-  {
-    href: "https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app",
-    title: "Docs",
-    description: "Find in-depth information about Next.js features and API.",
-  },
-  {
-    href: "https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app",
-    title: "Learn",
-    description:
-      "Learn about Next.js in an interactive course with&nbsp;quizzes!",
-  },
-  {
-    href: "https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app",
-    title: "Templates",
-    description:
-      "Discover and deploy boilerplate example Next.js&nbsp;projects.",
-  },
-  {
-    href: "https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app",
-    title: "Deploy",
-    description:
-      "Instantly deploy your Next.js site to a shareable URL with Vercel.",
-  },
-];
-
-export function Links() {
+export function Links({ items }) {
   return (
     <div>
-      {ITEMS.map((item) => {
+      {items.map((item) => {
         return (
           <a
             key={item.href}
